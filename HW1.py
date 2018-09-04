62f9e5ed27ba074ce7552a58a60d38cb7471f213
lizheng0420 <zheng-li-1@uiowa.edu>
2018-09-03 22:28:24 -0500

    first commit

---
+++ HW1.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# In[93]:
+
+
+import pandas as pd
+df= pd.read_csv('OriginalData.csv')
+df1=pd.melt(df, id_vars=["State"], var_name='year')
+cols1=list(df1.year)
+cols1 = [str(x)[:4] for x in cols1]
+cols2=list(df1.year)
+cols2=[str(x)[5:len(cols2)] for x in cols2]
+df1.year=cols1
+vl=list(df1.value)
+cols3=list(df1.value.iloc[765:1530])
+df1=df1.iloc[:765]
+df1['Marriage rate']=cols3
+df1['Marriage rate']=df1['Marriage rate'].apply( lambda x : str(x) + '%')
+df1.rename(columns= {'value':'Divorce rate'}, inplace=True)
+df1['Divorce rate']=df1['Divorce rate'].apply( lambda x : str(x) + '%')
+
